371bad2eeeca4ef773c4ec41ee3a98841a13777d
Christopher House <chris.house.00@gmail.com>
2022-12-03 08:09:24 -0600

    Test setup (#3)
    * removed comments
    
    * fixed pipeline
    
    * added test
    
    Co-authored-by: Chris House <chhouse@microsoft.com>

---
+++ Tests/ProductCatalogService.UnitTests/TestUnitTest.cs
@@ -0,0 +1,13 @@
+﻿using FluentAssertions;
+
+namespace ProductCatalogService.UnitTests;
+
+public class TestUnitTest
+{
+    [Fact]
+    public void True_Should_Be_True()
+    {
+        true.Should()
+            .BeTrue();
+    }
+}
